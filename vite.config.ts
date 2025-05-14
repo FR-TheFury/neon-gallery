e0abd3dadbf3ece5cd57a1242444486e3f204b5e
Téo Debay <frthefury@gmail.com>
2025-05-14 23:12:22 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/neon-gallery/',
   server: {
     host: "::",
     port: 8080,
